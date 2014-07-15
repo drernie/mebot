055896a14deedd10bd2015f6ba034a9a4c085cc5
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2014-07-15 16:44:58 -0700

    Rohan's first web app

--- 1-nothing/1-nothing.js
+++ 1-nothing/1-nothing.js
@@ -1,13 +1,28 @@
 if (Meteor.isClient) {
+	var favoriteFood = "Welcome";
+	var favoriteFoodDep = new Deps.Dependency;
+
+	var getFavoriteFood = function () {
+	  favoriteFoodDep.depend();
+	  return favoriteFood;
+	};
+
+	var setFavoriteFood = function (newValue) {
+	  favoriteFood = newValue;
+	  favoriteFoodDep.changed();
+	};
+
   Template.hello.greeting = function () {
-    return "Welcome to 1-nothing.";
+    return getFavoriteFood() + " to Rohan and Ernie's 1-nothing.";
   };
 
   Template.hello.events({
     'click input': function () {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      if (typeof console !== 'undefined') {
+        console.log("You pressed the button for " + getFavoriteFood());
+				setFavoriteFood("Goodbye");    	
+      }
     }
   });
 }
